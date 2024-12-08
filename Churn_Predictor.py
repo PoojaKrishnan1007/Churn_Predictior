0ce453694135ff710416447e30732672c147f0b8
Pooja <164362589+PoojaKrishnan1007@users.noreply.github.com>
2024-12-08 15:19:03 +0100

    Create Churn_Predictor.py

---
+++ Churn_Predictor.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import streamlit as st
+import joblib
+import numpy as np
+import os
+
+model_path = os.path.join(os.path.dirname(__file__), 'churn_prediction')
+model = joblib.load(model_path)
+def main():
+    st.title("Churn Predictor Model")
+    # Create input fields for features
+    CreditScore = st.number_input("Credit Score")
+    Geography = st.number_input("Geography")
+    Gender = st.number_input("Gender")
+    Age = st.number_input("Age")
+    Tenure = st.number_input("Tenure")
+    Balance = st.number_input("Balance")
+    NumOfProducts = st.number_input("Number of Products")
+    HasCrCard = st.number_input("Has Credit Card")
+    IsActiveMember = st.number_input("Is Active Member")
+    EstimatedSalary = st.number_input("Estimated Salary")
+        
+    # Create a dataframe from the input values
+    input_data = pd.DataFrame({
+    'CreditScore': [CreditScore],  
+    'Geography': [Geography], 
+    'Gender': [Gender],  
+    'Age': [Age],
+    'Tenure': [Tenure],
+    'Balance' : [Balance],
+    'NumOfProducts': [NumOfProducts], 
+    'HasCrCard': [HasCrcard], 
+    'IsActiveMember': [IsActiveMember],
+    'EstimatedSalary': [EstimatedSalary] 
+})
+    })
+    if st.button("Predict"):
+        with st.spinner('Calculating...'):  # Display a spinner while predicting
+            prediction = model.predict(input_data)
+            st.success(f"Predicted churn: {prediction[0]:,.2f}")  # Access the prediction value correctly
+            st.balloons()
+
+# Run the app
+if __name__ == "__main__":
+  main()
